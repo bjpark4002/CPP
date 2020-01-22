8d2d2a294bd58ce744e80ac17ec61135c70f587a
Bumjin Park <bjpark4002@gmail.com>
2020-01-21 23:37:50 -0800

    Copy constructor
    --

--- class.cpp
+++ class.cpp
@@ -2,6 +2,47 @@
 
 
 
+class SimpleTest{
+
+private:
+    int num1;
+    int num2;
+
+public:
+    SimpleTest(int a, int b) :num1(a), num2(b)
+    {
+       std::cout<<"constructor is invoked"<<std::endl; 
+    }
+
+
+    SimpleTest(SimpleTest & copy) : num1(copy.num1), num2(copy.num2)
+    {
+        std::cout<<"copy constructor is invoked"<<std::endl;
+    }
+    void showStatus(void){
+        std::cout<<"num1 : "<<num1<<" num2 : "<<num2<<std::endl;
+    }
+
+};
+
+int main(void){
+
+
+    SimpleTest *tem = new SimpleTest(10,15);
+    tem->showStatus();
+
+    SimpleTest *tem2 = tem;
+
+    tem2->showStatus();
+
+
+
+}
+
+
+
+/*
+
 class SimpleClass{
 
 private:
@@ -11,7 +52,7 @@ private:
 
 public:
     SimpleClass(int aa = 100, int bb = 200, int cc = 300)
-     : a(aa), b(bb), c(cc)
+     : a(aa), b(bb), c(cc)  //initializer.
     {
         std::cout<<"Constructor invoked"<<std::endl;
         // a = aa;
@@ -41,16 +82,27 @@ int main (void){
 
     std::cout<<" Test "<<std::endl;
 
-    /*
-    SimpleClass * ob = new SimpleClass;
+    // 
+    // SimpleClass * ob = new SimpleClass;
 
-    ob->InitMembers(1,2);
-    ob->ShowState();
-    */
+    // ob->InitMembers(1,2);
+    // ob->ShowState();
+    // 
 
-    SimpleClass * ob = new SimpleClass();
 
-    ob->ShowState();
+    // 
+    // SimpleClass * ob = new SimpleClass();
 
+    // ob->ShowState();
+    // 
+
+    int a(0);
+    int &aa(a);
+
+    aa+=10;
+
+    std::cout<< "a = "<<a<<" aa = "<<aa<<std::endl;
 
 }
+
+*/
