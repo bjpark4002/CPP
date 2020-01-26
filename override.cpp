52f5cca31033dfda2589ba64e79b6cd08aae77d5
Bumjin Park <bjpark4002@gmail.com>
2020-01-25 19:07:36 -0800

    update

--- override.cpp
+++ override.cpp
@@ -6,6 +6,9 @@ public:
     void MyFunc(){
         cout<<"First called\n";
     }
+    virtual ~First(){
+        cout<<"First Delete"<<endl;
+    }
 
 };
 
@@ -14,7 +17,9 @@ public:
     void MyFunc(){
         cout<<"Second called\n";
     }
-
+    ~Second(){
+        cout<<"Second Delete"<<endl;
+    }
 };
 
 class Third : public Second{
@@ -22,22 +27,27 @@ public:
     void MyFunc(){
         cout<<"Third called\n";
     }
-
+    ~Third(){
+        cout<<"Third Delete"<<endl;
+    }
 
 };
 
 int main(void){
 
 
-    Third * tptr = new Third();
-    Second * sptr=tptr;
-    First * fptr=sptr;
+    // Third * tptr = new Third();
+    // Second * sptr=tptr;
+    // First * fptr=sptr;
 
-    fptr->MyFunc();
-    sptr->MyFunc();
-    tptr->MyFunc();
-    delete tptr;
+    // fptr->MyFunc();
+    // sptr->MyFunc();
+    // tptr->MyFunc();
+    // delete tptr;
 
+    First * tem = new Third();
+
+    delete tem;
 
     return 0;
 }
